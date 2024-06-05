67e53cdfcef79fa389f6e75f26bcc2999d5000f8
shreypjain <jainshreyp@gmail.com>
2024-06-04 23:22:48 -0400

    openai compat server

--- src/llm.py
+++ src/llm.py
@@ -1,21 +1,10 @@
-import requests
+from openai import OpenAI
 import os
 
 API_KEY = os.getenv("BT_API_KEY")
+MODEL_ID = os.getenv("MODEL_ID")
 
-BASE_URL = "https://api.baseten.co/v1/models"
-MODEL_URL = f"https://model-{os.getenv("MODEL_ID")}.api.baseten.co/production/"
-
-def generate(input: str):
-    response = requests.post(
-        MODEL_URL + "/predict",
-        headers={"Authorization": f"Api-Key {API_KEY}"}
-        json=input
-    )
-    
-    return response.json()
-
-def all_models():
-    response = requests.get(
-        BASE_URL
-    )
+client = OpenAI(
+    api_key=API_KEY,
+    base_url=f"https://bridge.baseten.co/{MODEL_ID}/v1"
+)
